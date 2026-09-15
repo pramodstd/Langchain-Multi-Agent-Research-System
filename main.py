b55e74e0ec81b493d64bdf2d4e83491f451a3b4e
pramodstd <pmadulasa@gmail.com>
2026-09-15 16:38:43 +0530

    tools created

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.tools.tools import web_search, scrape_url
+
+
+# result1 = scrape_url.invoke("https://www.artificialintelligence-news.com")
+# print(result1)
+
+# result2 = web_search.invoke("Artificial Intelligence news")
+# print(result2)
+
